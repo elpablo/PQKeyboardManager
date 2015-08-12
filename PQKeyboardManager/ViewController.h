42052fdfaf650ded2a38593724729fd4488f5652
Paolo Quadrani <paolo.quadrani@gmail.com>
2015-08-12 14:38:40 +0200

    Initial Commit

---
+++ PQKeyboardManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PQKeyboardManager
+//
+//  Created by Paolo Quadrani on 12/08/15.
+//  Copyright (c) 2015 nEraLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PQKeyboardManager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PQKeyboardManager
+//
+//  Created by Paolo Quadrani on 12/08/15.
+//  Copyright (c) 2015 nEraLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
